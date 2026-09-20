6defc28c64c8235f5e06478b1f7f9a780d33650e
kritikaverma01 <Kritikav807@gmail.com>
2026-09-20 11:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeColor() {
+  const select = document.getElementById("colorSelect");
+
+  if (select.selectedIndex !== -1) {
+    select.remove(select.selectedIndex);
+  }
+}
+
+const button = document.querySelector('input[type="button"]');
+button.addEventListener("click", removeColor);
